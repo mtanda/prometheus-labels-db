3385d936fdf9b8eb947de17827e909ff2aad1f13
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2025-02-28 18:19:51 +0900

    add recorder

---
+++ internal/recorder/recorder.go
@@ -0,0 +1,48 @@
+package recorder
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/mtanda/prometheus-labels-db/internal/database"
+	"github.com/mtanda/prometheus-labels-db/internal/model"
+)
+
+const (
+	MaxRetry = 3
+)
+
+type Recorder struct {
+	ldb       *database.LabelDB
+	metricsCh chan model.Metric
+}
+
+func New(ldb *database.LabelDB, ch chan model.Metric) *Recorder {
+	return &Recorder{
+		ldb:       ldb,
+		metricsCh: ch,
+	}
+}
+
+func (r *Recorder) Run(wg *sync.WaitGroup) {
+	ctx := context.TODO()
+	wg.Add(1)
+	go func() {
+		for metric := range r.metricsCh {
+			for i := 0; i < MaxRetry; i++ {
+				err := r.ldb.RecordMetric(ctx, metric)
+				if err != nil {
+					fmt.Printf("failed to record metric: %v, %v\n", metric, err)
+				} else {
+					break
+				}
+			}
+		}
+		wg.Done()
+	}()
+}
+
+func (r *Recorder) Stop() {
+	close(r.metricsCh)
+}
---
+++ internal/recorder/recorder_test.go
@@ -0,0 +1,58 @@
+package recorder
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/mtanda/prometheus-labels-db/internal/database"
+	"github.com/mtanda/prometheus-labels-db/internal/model"
+	"github.com/prometheus/prometheus/model/labels"
+)
+
+func TestRecord(t *testing.T) {
+	ctx := context.Background()
+	chanLength := 10
+	metricsCount := chanLength * 2
+
+	var wg sync.WaitGroup
+	dbDir := t.TempDir()
+	ldb, err := database.Open(dbDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	metricsCh := make(chan model.Metric, chanLength)
+	recorder := New(ldb, metricsCh)
+	recorder.Run(&wg)
+
+	now := time.Now().UTC()
+	from := now.Add(-1 * time.Hour)
+	to := now
+	for i := 0; i < metricsCount; i++ {
+		metricsCh <- model.Metric{
+			Namespace:  "test_namespace",
+			MetricName: "test_name",
+			Region:     "test_region",
+			Dimensions: model.Dimensions{
+				{
+					Name:  "dim1",
+					Value: fmt.Sprintf("dim_value%d", i),
+				},
+			},
+			FromTS:    from,
+			ToTS:      to,
+			UpdatedAt: now,
+		}
+	}
+	recorder.Stop()
+	wg.Wait()
+
+	result, err := ldb.QueryMetrics(ctx, from, to, []*labels.Matcher{
+		labels.MustNewMatcher(labels.MatchEqual, "namespace", "test_namespace"),
+	})
+	if len(result) != metricsCount {
+		t.Fatalf("unexpected metrics count: %d", len(result))
+	}
+}
