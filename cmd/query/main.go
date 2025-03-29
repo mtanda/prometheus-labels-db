208b6a8919a03c54216ee41ba059a3633a0b1971
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2025-03-30 07:42:19 +0900

    add query command

---
+++ cmd/query/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/mtanda/prometheus-labels-db/internal/database"
+	"github.com/prometheus/prometheus/promql/parser"
+)
+
+func seriesHandler(w http.ResponseWriter, r *http.Request, db *database.LabelDB) {
+	query := r.URL.Query()
+	matchParam := query["match[]"]
+	matchers, err := parser.ParseMetricSelectors(matchParam)
+	if err != nil {
+		http.Error(w, "invalid match[] parameter: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	startParam := query.Get("start")
+	endParam := query.Get("end")
+	start, err := time.ParseInLocation(time.RFC3339, startParam, time.UTC)
+	if err != nil {
+		http.Error(w, "failed to parse start timestamp: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	end, err := time.ParseInLocation(time.RFC3339, endParam, time.UTC)
+	if err != nil {
+		http.Error(w, "failed to parse end timestamp: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.Background()
+
+	data := []map[string]string{}
+	for _, matcher := range matchers {
+		metrics, err := db.QueryMetrics(ctx, start, end, matcher)
+		if err != nil {
+			http.Error(w, "failed to query metrics: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		for _, metric := range metrics {
+			data = append(data, metric.Labels())
+		}
+	}
+
+	response := map[string]interface{}{
+		"status": "success",
+		"data":   data,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	var dbDir string
+	flag.StringVar(&dbDir, "db.dir", "./data/", "Path to the database directory")
+	var listenAddress string
+	flag.StringVar(&listenAddress, "web.listen-address", "0.0.0.0:8080", "Address to listen")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	slog.SetDefault(logger)
+
+	db, err := database.Open(dbDir)
+	if err != nil {
+		slog.Error("failed to open database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/api/v1/series", func(w http.ResponseWriter, r *http.Request) {
+		seriesHandler(w, r, db)
+	})
+	http.ListenAndServe(listenAddress, nil)
+}
--- go.mod
+++ go.mod
@@ -26,11 +26,18 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.17 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dennwc/varint v1.0.0 // indirect
 	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_golang v1.21.0-rc.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.36.4 // indirect
 )
