a8f2bb1e661cdb6ec066e2f3acfb3160a41e5358
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2025-05-03 00:18:32 +0900

    set default region if no region is specified

--- internal/model/config.go
+++ internal/model/config.go
@@ -1,8 +1,12 @@
 package model
 
 import (
+	"context"
 	"os"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -27,5 +31,43 @@ func LoadConfig(configFile string) (*Config, error) {
 		return nil, err
 	}
 
+	for i, target := range cfg.Targets {
+		if target.Region == "" {
+			region, err := getDefaultRegion()
+			if err != nil {
+				return nil, err
+			}
+			cfg.Targets[i].Region = region
+		}
+	}
+
 	return &cfg, nil
+}
+
+var defaultRegion string
+
+func getDefaultRegion() (string, error) {
+	if defaultRegion != "" {
+		return defaultRegion, nil
+	}
+
+	envRegion := os.Getenv("AWS_REGION")
+	if envRegion != "" {
+		defaultRegion = envRegion
+		return defaultRegion, nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return "", err
+	}
+	client := imds.NewFromConfig(cfg)
+	region, err := client.GetRegion(ctx, &imds.GetRegionInput{})
+	if err != nil {
+		return "", err
+	}
+	defaultRegion = region.Region
+	return defaultRegion, nil
 }
